4c4ba8597f3afc2baec1848b83851fcb9c7aecf5
ho62zoq <fabian.hofer@uni-jena.de>
2025-02-26 11:06:11 +0100

    fix: seaborn pdf

--- code/src/pairwise_preference/evaluation/seaborn_plot.py
+++ code/src/pairwise_preference/evaluation/seaborn_plot.py
@@ -51,13 +51,17 @@ for model_name, df in data_dict.items():
     g.ax.set_xlim(0, 1)
     g.ax.set_ylim(0, global_max_y)
 
+    # Set y-axis to scientific notation
+    g.ax.ticklabel_format(style='scientific', axis='y', scilimits=(0,0))
+
     # Labels and title
-    g.ax.set_xlabel('Score', fontsize=14)
-    g.ax.set_ylabel('Absolute Frequency', fontsize=14)
+    g.ax.set_xlabel('Score', fontsize=24)
+    g.ax.set_ylabel('Absolute Frequency', fontsize=24)
     # g.ax.set_title(f'Distribution of relevance scores generated using {model_name}', 
     #                fontsize=16, weight="bold", loc="center")
     g.ax.grid(True, linestyle="--", alpha=0.6)
+    g.tick_params(labelsize=16)
 
     # Save the plot instead of showing it
-    plt.savefig(f'pairwise_score_distribution_{model_name}.png', bbox_inches='tight')
+    plt.savefig(f'pairwise_score_distribution_{model_name}.pdf', bbox_inches='tight')
     plt.close()
