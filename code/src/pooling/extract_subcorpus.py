db176bd14aa36d666ae78bfc7f1bd54b708561a7
Maik Froebe <maik.froebe@uni-jena.de>
2025-03-02 11:12:29 +0100

    mf

--- code/src/pooling/extract_subcorpus.py
+++ code/src/pooling/extract_subcorpus.py
@@ -3,49 +3,38 @@ import copy
 import gzip
 import json
 import os
+from tqdm import tqdm
 from base64 import b64encode
 from pathlib import Path
 from typing import Any, Iterable, Optional
 
 
-def write_lines_to_file(self, lines: Iterable[str], path: Path) -> None:
-    if path.exists():
-        raise RuntimeError(f"File already exists: {path}")
-    path.parent.mkdir(parents=True, exist_ok=True)
-
+def write_lines_to_file(lines: Iterable[str], path: Path) -> None:
     if os.path.abspath(path).endswith(".gz"):
-        with gzip.open(os.path.abspath(path), "wb") as file:
+        with gzip.open(os.path.abspath(path), "ab") as file:
             for line in lines:
                 if not line:
                     continue
                 file.write((line + "\n").encode("utf-8"))
+                file.flush()
     else:
         with path.open("wt") as file:
             file.writelines("%s\n" % line for line in lines if line)
 
-def yield_docs(self, dataset, include_original, skip_duplicate_ids, allowlist_path_ids):
+def yield_docs(dataset, include_original, skip_duplicate_ids, allowed_ids):
     already_covered_ids = set()
-    allowed_ids = set()
-    if allowlist_path_ids:
-        with open(allowlist_path_ids, "r") as inp_file:
-            for i in inp_file:
-                allowed_ids.add(i.strip())
-        print("I use a allow list of size ", len(allowed_ids))
-
     docs_store = dataset.docs_store()
 
     for doc_id in tqdm(allowed_ids, "Load Documents"):
         doc = docs_store.get(doc_id)
         if skip_duplicate_ids and doc.doc_id in already_covered_ids:
             continue
-        if allowlist_path_ids and str(doc.doc_id) not in allowed_ids:
-            continue
 
-        yield self.map_doc(doc, include_original)
+        yield map_doc(doc, include_original)
         if skip_duplicate_ids:
             already_covered_ids.add(doc.doc_id)
 
-def map_doc(self, doc: tuple, include_original=True) -> str:
+def map_doc(doc: tuple, include_original=True) -> str:
     """Maps a document of any dataset (loaded through ir_datasets) to a standarized format
     stores full document data too, if flag 'include_original' is set
 
@@ -60,6 +49,9 @@ def map_doc(self, doc: tuple, include_original=True) -> str:
 
 
 if __name__ == '__main__':
+    from ir_datasets import load
+    from ir_datasets_clueweb22 import register
+    register()
     DATA_DIR = '/mnt/ceph/storage/data-in-progress/data-teaching/theses/thesis-fhofer/data'
     PATHS = [
         'argsme/2020-04-01/touche-2020-task-1/clueweb22/b/candidates-chatnoir/doc_ids.jsonl',
@@ -70,13 +62,28 @@ if __name__ == '__main__':
         'msmarco-passage/trec-dl-2020/judged/clueweb22/b/candidates-chatnoir/doc_ids.jsonl'
     ]
 
+    OUTPUT_FILE = f'{DATA_DIR}/clueweb22-transfer/documents.jsonl.gz'
     doc_ids = set()
+    dataset = load("clueweb22/b")
+    already_covered_ids = set()
 
+    with gzip.open(OUTPUT_FILE, 'rt') as f:
+        for l in f:
+            try:
+                l = json.loads(l)
+                already_covered_ids.add(l['docno'])
+            except:
+                pass
+
+    print(f'skip {len(already_covered_ids)}')
     for json_file in PATHS:
         with open(f'{DATA_DIR}/{json_file}', 'r') as f:
             for l in f:
                 l = json.loads(l)
                 for doc_id in l['doc_ids']:
-                    doc_ids.add(doc_id)
+                    if doc_id not in already_covered_ids:
+                        doc_ids.add(doc_id)
     print(len(doc_ids))
-    
+
+    docs = yield_docs(dataset, False, True, doc_ids)
+    write_lines_to_file(docs, OUTPUT_FILE)
